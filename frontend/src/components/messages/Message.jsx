aa2dd11b28bf6cd5293df261bd9ee758302890a6
vishal <vishalvis3333@gmail.com>
2024-06-06 19:55:49 +0530

    changes

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -17,7 +17,7 @@ const Message = ({ message }) => {
 		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				<img alt='Tailwind CSS chat bubble component' src={profilePic} />
 				</div>
 			</div>
 			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
