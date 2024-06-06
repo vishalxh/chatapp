744d6160652d0178b7fe0f4f8ae09a3caac1a1c0
vishal <vishalvis3333@gmail.com>
2024-06-06 19:14:25 +0530

    changes

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -5,26 +5,25 @@ const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
 
-		
-		jwt.verify(token,process.env.JWT_SECRET, async (err, decodedToken) => {
-            if(err){
-            console.log(err.message);
-            res.redirect('/login');
-            }
-            else{
-                console.log(decodedToken);
-                const user = await User.findById(decodedToken.userId).select("-password");
+		if (!token) {
+			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
+		}
 
-				if (!user) {
-					return res.status(404).json({ error: "User not found" });
-				}
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-				req.user = user;
+		if (!decoded) {
+			return res.status(401).json({ error: "Unauthorized - Invalid Token" });
+		}
 
-				next();
-            }
-        })
-		
+		const user = await User.findById(decoded.userId).select("-password");
+
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		req.user = user;
+
+		next();
 	} catch (error) {
 		console.log("Error in protectRoute middleware: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
