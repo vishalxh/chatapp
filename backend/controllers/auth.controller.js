8fdc31804651e9df15ded8d43cba6fbdecf06e4d
vishal <vishalvis3333@gmail.com>
2024-06-06 18:37:44 +0530

    changes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,8 +22,8 @@ export const signup = async (req, res) => {
 
 		// https://avatar-placeholder.iran.liara.run/
 
-		const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+		const boyProfilePic = `https://api.dicebear.com/8.x/avataaars/svg?seed=${username}`;
+		const girlProfilePic = `https://api.dicebear.com/8.x/avataaars/svg?seed=${username}`;
 
 		const newUser = new User({
 			fullName,
